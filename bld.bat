6a2a30d0ae814b5ffbdef07e19c4232e9af6fa58
Steve Ford <sford@geeky-boy.com>
2026-02-16 12:54:55 -0500

    updates

--- bld.bat
+++ bld.bat
@@ -1,5 +1,5 @@
 @echo off
 rem bld.bat
 
-cl /std:c11 /W4 /O2 /MT /nologo dual_cap.c re.c plat_win.c ws2_32.lib /Fe:dual_cap.exe
+cl /std:c11 /W4 /O2 /MT /nologo /D_CRT_SECURE_NO_WARNINGS /D_CRT_NONSTDC_NO_DEPRECATE dual_cap.c re.c plat_win.c ws2_32.lib /Fe:dual_cap.exe
 exit /b %ERRORLEVEL%
